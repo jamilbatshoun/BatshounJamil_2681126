5ae8bbe3362fbb8e4ed9b0389fb77e0c820cb4ac
jamilbatshoun <jamilbatshoun@gmail.com>
2016-09-19 12:06:42 -0700

    Update Gaddis_8thEd_Chap2_#3.cpp

--- Gaddis_8thEd_Chap2_#3.cpp
+++ Gaddis_8thEd_Chap2_#3.cpp
@@ -6,17 +6,17 @@
  * Purpose: Chapter 3 #3: Sales Tax
  */
 
-#include <cstdlib>
+
 #include <iostream>
 using namespace std;
 
 
 int main(int argc, char** argv) {
-    int purPrice=95;
-    float tax= .06;
-    int salesTax;
+    int purPrice=95; //setting the purchase price variable (purPrice) equal to 95
+    float tax= .06;  //including the state and county sales tax together
+    int salesTax;    //the product of the sales tax percentage and the money spent
     
-    salesTax=purPrice*tax;
+    salesTax=purPrice*tax; //relevent equation
     
     cout<<"The total sales tax of a purchase of $95 with a 4% state sales tax "
             "and a 2% county sales tax is $"<<salesTax<<endl;
