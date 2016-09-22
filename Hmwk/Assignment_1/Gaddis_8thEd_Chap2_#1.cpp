37e956744a641b1731aeb433cdd42f083698e51b
jamilbatshoun <jamilbatshoun@gmail.com>
2016-09-21 23:16:51 -0700

    Add files via upload

---
+++ Hmwk/Assignment_1/Gaddis_8thEd_Chap2_#1.cpp
@@ -0,0 +1,30 @@
+/*
+ * File:   main.cpp
+ * Author: Jamil Batshoun
+ *
+ * Created on September 14, 2016, 12:18 PM
+ * Purpose: Chapter 2 programming challenges #1: Compute the sum of 50 and 100.
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    //Create three variables to store the integers 50 and 100 and their sum.
+    int x, y, total;
+    
+    //Determine the value of the three variables.
+    x=50;
+    y=100;
+    total=x+y;
+    
+    //Output the sum of x and y.
+    cout<<" The sum of 50 and 100 is "<<total<<"."<<endl;
+    
+    
+    return 0;
+}
+
