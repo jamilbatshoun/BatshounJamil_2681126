4b76650a17d0fca0c3e49b5fba3cdecc31a5700b
rcc <rcc>
2016-09-26 10:00:21 -0700

    Signed-off-by: Jamil Batshoun

---
+++ Lab/truth table.cpp
@@ -0,0 +1,55 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on September 26, 2016, 9:35 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+    bool x,y;
+    
+  
+    cout<<"X Y !X !Y X&&Y X||Y X^Y X^Y^Y X^Y^X !(X&&Y) !X||!Y !(X||Y) !X&&!Y"<<endl;
+    
+    x=true;
+    y=true;
+    
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<" ";
+    cout<<(!x?'T':'F')<<" ";
+    cout<<(!y?'T':'F')<<" ";
+    cout<<(x&&y?'T':'F')<<" ";
+    cout<<(x||y?'T':'F')<<" ";
+    cout<<(x^y?'T':'F')<<" ";
+    cout<<(x^y^y?'T':'F')<<" ";
+    cout<<(x^y^x?'T':'F')<<" ";
+    cout<<(!(x&&y)?'T':'F')<<" ";
+    cout<<(!x||y?'T':'F')<<" ";
+    cout<<(!(x||y)?'T':'F')<<" ";
+    cout<<(!x&&!y?'T':'F')<<" \n \n";
+    
+
+    y=false;
+    
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<" ";
+    cout<<(!x?'T':'F')<<" ";
+    cout<<(!y?'T':'F')<<" ";
+    cout<<(x&&y?'T':'F')<<" ";
+    cout<<(x||y?'T':'F')<<" ";
+    cout<<(x^y?'T':'F')<<" ";
+    cout<<(x^y^y?'T':'F')<<" ";
+    cout<<(x^y^x?'T':'F')<<" ";
+    cout<<(!(x&&y)?'T':'F')<<" ";
+    cout<<(!x||y?'T':'F')<<" ";
+    cout<<(!(x||y)?'T':'F')<<" ";
+    cout<<(!x&&!y?'T':'F')<<" ";
+    return 0;
+}
+
