0817a7b431b6da9d83d1357d8cbfafdfb0e257a8
rcc <rcc>
2016-09-28 19:47:01 -0700

    Signed-off-by: jb

--- Hmwk/Assignment_2/Gaddis_8thEd_Chap3_#1.cpp
+++ Hmwk/Assignment_2/Gaddis_8thEd_Chap3_#1.cpp
@@ -1,17 +1,27 @@
 /* 
  * File:   main.cpp
- * Author: Jamil Batshoun
+ * Author: rcc
  *
- * Created on September 25, 2016, 11:19 PM
+ * Created on September 28, 2016, 6:47 PM
  */
 
 #include <iostream>
 
 using namespace std;
 
-
 int main(int argc, char** argv) {
-
+    
+    int gasTank;
+    int miles;
+    cout <<"Please enter the capacity of the gas tank."<< endl;
+    
+    cin >> gasTank;
+    
+    cout <<"How many miles can the car be driven on a full tank?"<<endl;
+    
+    cin>> miles;
+    
+    cout<<"The car gets "<<miles/gasTank<<" miles per gallon."<<endl;
     return 0;
 }
 
