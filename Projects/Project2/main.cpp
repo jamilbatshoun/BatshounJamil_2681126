1b595365d9f8df5241706482a5aaacaf5cb3ca81
Jamil Batshoun <Jamil Batshoun>
2016-12-03 00:05:46 -0800

    Signed-off-by: Jamil Batshoun <Jamil Batshoun>

---
+++ Projects/Project2/main.cpp
@@ -0,0 +1,17 @@
+
+
+/* 
+ * File:   main.cpp
+ * Author: Jamil Batshoun
+ * Created on December 3, 2016, 12:05 AM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
